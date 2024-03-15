2a8f33ebeb9801fc0b734a29e3d921b62a77bf55
Lucas Silva <lucas.nascimento@gmail.com>
2024-03-14 22:53:20 -0700

    Add a list of todos that loads from the go back end as well as a header that contains some of the functionalities I would like to implement

--- main.go
+++ main.go
@@ -23,6 +23,33 @@ func SetUpPages() Pages {
 	return pages
 }
 
+type Todo struct {
+	Title string `json:"title"`
+	Done  bool   `json:"done"`
+	ID    int    `json:"id"`
+}
+
+var id int = 0
+
+func NewTodo(title string, done bool) *Todo {
+	todo := &Todo{
+		Title: title,
+		Done:  done,
+		ID:    id,
+	}
+
+	id++
+
+	return todo
+}
+
+type TodoList []*Todo
+
+var todos = TodoList{
+	NewTodo("Do the dishes", false),
+	NewTodo("Do the laundry", false),
+}
+
 func main() {
 	// Set up pages
 	SetUpPages()
@@ -30,6 +57,9 @@ func main() {
 	// Create a new instance of Echo
 	e := echo.New()
 	e.Renderer = library.NewTemplates()
+
+	e.Static("/css", "css")
+	e.Static("/images", "images")
 
 	// middleware
 	e.Use(middleware.Logger())
@@ -45,6 +75,8 @@ func main() {
 // handler
 func indexHandler(c echo.Context) error {
 	page := pages["index"]
+
+	page.Data = todos
 
 	return c.Render(http.StatusOK, page.TemplateName, page)
 }
